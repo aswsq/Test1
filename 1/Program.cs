c1d87a7945b8488f4185a257cadfc269b647d3c5
aswsq <72857666+aswsq@users.noreply.github.com>
2024-11-29 16:11:48 +0300

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿const double KursDollar = 0.011;
+double KolvoRubli, result;
+string str = " ";
+Console.WriteLine("Kolvo rubli");
+KolvoRubli = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(KolvoRubli*KursDollar);
+
+
+
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b, result;
+        Console.WriteLine("Введите число 1");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите число 2");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите действие");
+        ConsoleKey consoleKey = Console.ReadKey().Key;
+        switch (consoleKey)
+        {
+            case ConsoleKey.OemPlus:
+                result = a + b;
+                Console.WriteLine(result);
+            default:
+                break;
+        }
+    }
+}
